2d38e73f6ab7bbd6d38ec17ff0d059714bb7cccb
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-06-05 09:28:35 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -0,0 +1,20 @@
+def solution(board):
+    answer = 0 
+    n = len(board)
+    
+    moves = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
+    result = [[0 for i in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                result[i][j] = -1
+                for move in moves:
+                    x = i - move[0]
+                    y = j - move[1]
+                    if x>=0 and y>=0 and x<n and y<n:
+                        result[x][y] = -1
+    for i in range(n):
+        for j in range(n):
+            if result[i][j] == 0:
+                answer += 1
+    return answer
