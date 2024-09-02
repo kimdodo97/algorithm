1caf654a91bf92e8325ea6d59cefef2bf7abbaba
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-09-02 22:41:54 +0900

    [level 2] Title: 가장 큰 수, Time: 1403.74 ms, Memory: 26.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    answer = ''
+    numbers = list(map(str,numbers))
+    numbers.sort(key = lambda x:x*4, reverse=True)
+    answer = str(int(''.join(numbers)))
+    return answer
