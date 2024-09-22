8f4213fbbebdc67719694f5ff0adb8c0028aa2ca
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-09-22 18:46:19 +0900

    [Silver III] Title: N과 M (3), Time: 584 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+def recursive(num):
+    if num == m:
+        print(' '.join(arr))
+        return
+
+    for i in range(1,n+1):
+        arr.append(str(i))
+        recursive(num + 1)
+        arr.pop()
+
+n,m = map(int,input().split())
+arr = []
+recursive(0)
